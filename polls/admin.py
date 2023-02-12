59f234eeabaddae15f6bb41940bc2bfe26fb7110
ruhulfbr <ruhul11bd@gmail.com>
2023-02-13 01:01:55 +0600

    Learn something and complete poll projects

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    list_display = ('pub_date', 'question_text', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'question', 'votes')
+    list_filter = ['question']
+    search_fields = ['choice_text']
+
+
+admin.site.register(Choice, ChoiceAdmin)
 
